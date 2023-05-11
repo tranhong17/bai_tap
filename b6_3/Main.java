048d3b0b16f711157c3fd35b9fbeed895682c724
tranhong17 <132926472+tranhong17@users.noreply.github.com>
2023-05-11 10:01:01 +0700

    Update Main.java

--- b6_3/Main.java
+++ b6_3/Main.java
@@ -4,13 +4,14 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Movable> mo = new ArrayList<>();
-        MovablePoint m = new MovablePoint(2,5, 5,6);
-        mo.add(m);
-
-
-        for(Movable b: mo){
-            System.out.println(b);
-        }
+        MovablePoint p = new MovablePoint(0,0,2,1);
+        p.moveUp();
+        System.out.println(p);
+        p.moveRight();
+        System.out.println(p);
+        p.moveDown();
+        System.out.println(p);
+        p.moveLeft();
+        System.out.println(p);
     }
 }
